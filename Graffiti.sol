66529435865ae31a2e891d9a84cee7c53fc517a5
RaidasGrisk <mrraidas@gmail.com>
2021-06-16 14:35:09 +0300

    refactor to save on gas

--- Graffiti.sol
+++ Graffiti.sol
@@ -8,50 +8,53 @@ pragma solidity >=0.7.0 <0.9.0;
  */
 contract GraffitiWall {
 
-    // size of the wall in pixels
-    uint x_length = 10;
-    uint y_length = 10;
 
-    // pixel struct with RGB model
+    // Not initializing a fixed size wall as this will
+    // introduce unnecesary deployment costs by reserving
+    // a lot of empty space (coords x, y with 0, 0, 0 RGB values)
+    // on the blockchain.
+
+    // Instead lets not assume the size of the wall beforehand
+    // It can be infinate. Newly created pixels are simply pused
+    // to an array and hence something is introduced to the wall
+
+    // pixel struct holding the values of
+    // coordinates (x, y) and RGB model
     struct Pixel {
-        uint r;
-        uint g;
-        uint b;
+        uint256 x;
+        uint256 y;
+        uint8 r;
+        uint8 g;
+        uint8 b;
     }
 
     // this is the GraffitiWall
-    // where each pixel in the wall
-    // has a coordinates and a Pixel
-    // with RGB values stored permanently
-    // on the blockhain
-    Pixel[10][10] pixels;
+    // permanently stored on the blockchain
+    // by default, the wall is empty
+    Pixel[] public pixels;
 
-    // initiallizing the wall with whitness
-    constructor() {
-        for (uint x = 0; x < x_length; x++) {
-            for (uint y = 0; y < y_length; y++) {
-                pixels[x][y] = Pixel(0, 0, 0);
-            }
-        }
-    }
-
-    // getter for the front-end
-    function show() public view returns(Pixel[10][10] memory){
+    // getter for the front-end that returns
+    // all pixels pushed to the wall
+    // Should consider implementing pagination pattern
+    // in case this is actually used
+    // https://programtheblockchain.com/posts/2018/04/20/storage-patterns-pagination/
+    function show() public view returns(Pixel[] memory){
         return pixels;
     }
 
-    // set the RGB values of selected pixels
-    // the argument is an array of arrays where:
-    // [[x, y, R, G, B], ..., [x, y, R, G, B]]
-    function paint(uint[5][] calldata args) public {
-
-        for (uint i = 0; i < args.length; i++) {
-            uint x = args[i][0];
-            uint y = args[i][1];
-            uint r = args[i][2];
-            uint g = args[i][3];
-            uint b = args[i][4];
-            pixels[x][y] = Pixel(r, g, b);
+    // as explained before, the pixels array is empty by default
+    // the following pushes pixels to this array and these pixels will be displayed
+    function paint(uint256[2][] calldata _coords, uint8[3][] calldata _rgbColor) public {
+        for (uint256 i = 0; i < _coords.length; i++) {
+            pixels.push(
+                Pixel(
+                    _coords[i][0],
+                    _coords[i][1],
+                    _rgbColor[i][0],
+                    _rgbColor[i][1],
+                    _rgbColor[i][2]
+                )
+            );
         }
     }
 }
